f2fc2dd988299d19b74599ba05ce0eda6f9f3ae9
danielmoi <daniel@ampersandmoi.com>
2016-03-25 12:06:15 +1100

    Create users route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
+# == Route Map
+#
+#      Prefix Verb URI Pattern            Controller#Action
+# users_index GET  /users/index(.:format) users#index
+#  users_edit GET  /users/edit(.:format)  users#edit
+#   users_new GET  /users/new(.:format)   users#new
+#        root GET  /                      pages#home
+#
+
 Rails.application.routes.draw do
-  
+
   root :to => 'pages#home'
+
+  # User routes
+  resources :users, :only => [:new, :create, :index, :update]
+  get '/signup' => 'users#new'
+
 
 end
