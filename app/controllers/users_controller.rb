141e0b356b4c56b85abec5964b6e4865f4a5e1b6
danielmoi <daniel@ampersandmoi.com>
2016-03-25 15:28:37 +1100

    Handle submit from signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,25 @@
 class UsersController < ApplicationController
 
+  # render view for users/new
   def new
     @user = User.new
   end
+
+  # receive data from users/new (the form)
+  def create
+    @user = User.new user_params
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
   
+
+  # can't visit these URLs
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :image)
+  end
+
 end
