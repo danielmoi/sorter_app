cb5a63fdcce8a197ff3783af6023af0f700ce2a5
danielmoi <daniel@ampersandmoi.com>
2016-03-26 10:52:05 +1100

    Handle submit from login page
    This is the session#create action

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,4 +1,30 @@
 class SessionController < ApplicationController
+  # display login page
   def new
   end
+
+  # handle submit from login page
+  def create
+    user = User.find_by :email => params[:email]
+
+    # if a user, with that email, is in the DB
+    # then apply the authenticate method
+    # if successful authentication...
+    if user.present? && user.authenticate(params[:password])
+
+      # set the user_id in session hash to the user's id
+      session[:user_id] = user.id
+      # then go to home page (with that id saved in session)
+      redirect_to root_path
+
+    # if user isn't in DB or authentication fails...
+    else
+      # set :error in flash to this string
+      flash[:error] = 'Invalid email or password' # deliberate ambiguity
+      # then go to login page
+      redirect_to login_path
+    end
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,30 @@
 # == Route Map
 #
-#      Prefix Verb URI Pattern            Controller#Action
-# users_index GET  /users/index(.:format) users#index
-#  users_edit GET  /users/edit(.:format)  users#edit
-#   users_new GET  /users/new(.:format)   users#new
-#        root GET  /                      pages#home
+#   Prefix Verb  URI Pattern          Controller#Action
+#     root GET   /                    pages#home
+#    users GET   /users(.:format)     users#index
+#          POST  /users(.:format)     users#create
+# new_user GET   /users/new(.:format) users#new
+#     user PATCH /users/:id(.:format) users#update
+#          PUT   /users/:id(.:format) users#update
+#   signup GET   /signup(.:format)    users#new
+#    login GET   /login(.:format)     session#new
 #
 
 Rails.application.routes.draw do
 
   root :to => 'pages#home'
 
-  # User routes
+  ## User routes
   resources :users, :only => [:new, :create, :index, :update]
   get '/signup' => 'users#new'
 
-  # Session routes
+  ## Session routes
+  # Display login view
   get '/login' => 'session#new'
+
+  # Receive login submit
+  post '/login' => 'session#create'
 
 
 end
