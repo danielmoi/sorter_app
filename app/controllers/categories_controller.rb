fb7c1e8d83ead414e3c322525cf8714b0731aa92
danielmoi <daniel@ampersandmoi.com>
2016-03-29 16:23:52 +1100

    Add new category functionality

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,16 @@
 class CategoriesController < ApplicationController
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new category_params
+    @category.user_id = @current_user.id
+    if @category.save
+      redirect_to categories_path
+    else
+      render :new
+    end
   end
 
   def index
@@ -16,6 +27,12 @@ class CategoriesController < ApplicationController
   end
 
   def delete
+  end
+
+  # helper methods
+  private
+  def category_params
+    params.require(:category).permit(:name)
   end
 
 
