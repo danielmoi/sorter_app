bc2a11bcde0da873a83097a234ea340ebca770ab
danielmoi <daniel@ampersandmoi.com>
2016-03-30 16:50:36 +1100

    Fix graph data

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -13,8 +13,8 @@ class GraphsController < ApplicationController
   def data2
     my_arr = []
     User.all.each do |user|
-      # my_arr << [user.email, user.categories.size]
-      my_arr << user.categories.size
+      my_arr << [user.email, user.categories.size]
+      # my_arr << user.categories.size
     end
 
     respond_to do |format|
