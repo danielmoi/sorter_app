55c46897b3c0be981c8eacab6d3627e1eeeb0d28
danielmoi <daniel@ampersandmoi.com>
2016-03-31 10:50:43 +1100

    Populate graph with user name and counts

--- app/assets/javascripts/graph2.js
+++ app/assets/javascripts/graph2.js
@@ -10,22 +10,3 @@ var myData = $.ajax({
 
   }
 });
-
-
-var barChartData = {
-  labels : myData.responseJSON.labels,
-  datasets : [
-    {
-      fillColor : "rgba(220,220,220,0.5)",
-      strokeColor : "rgba(220,220,220,0.8)",
-      highlightFill: "rgba(220,220,220,0.75)",
-      highlightStroke: "rgba(220,220,220,1)",
-      data : myData.responsJSON.counts
-    },
-  ]
-};
-var ctx = document.getElementById('chart1').getContext('2d');
-
-var myChart = new Chart(ctx).Bar(barChartData, {
-  responsive: true
-});
