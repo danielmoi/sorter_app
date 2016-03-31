d54ec35b7db928fcbfce82c27d311ceed377561a
danielmoi <daniel@ampersandmoi.com>
2016-03-31 15:07:39 +1100

    Cycle greeting on home page!

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,7 @@
 class PagesController < ApplicationController
   def home
-    welcome_arr = [ "Mirëdita",  "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý", "Goddag", "Goede", "Hello", "Saluton", "Hei", "Bonjour", "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Jó napot", "Góðan daginn", "Halo", "Qanuipit?", "Dia dhuit", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salve", "Ni hao", "Hallo", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hej", "Sa-wat-dee", "Merhaba", "Vitayu", "Xin chào", "Sut Mae?", "Sholem Aleychem", "Sawubona" ]
+    @welcome_arr = [ "Mirëdita",  "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý", "Goddag", "Goede", "Hello", "Saluton", "Hei", "Bonjour", "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Jó napot", "Góðan daginn", "Halo", "Qanuipit?", "Dia dhuit", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salve", "Ni hao", "Hallo", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hej", "Sa-wat-dee", "Merhaba", "Vitayu", "Xin chào", "Sut Mae?", "Sholem Aleychem", "Sawubona" ]
 
-    random = Random.rand(0..41)
-    @greeting = welcome_arr[random]
 
   end
 end
